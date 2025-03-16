f89e04858bd8fb6942cb93ad916e0d286a9079ea
Tanha1627 <151685331+Tanha1627@users.noreply.github.com>
2025-03-17 01:54:45 +0600

    Add files via upload

--- OnlineBookstore/Navbar.jsx
+++ OnlineBookstore/Navbar.jsx
@@ -1,72 +1,94 @@
-import React, { useState } from 'react'
-import { Link, Links } from 'react-router-dom'
-import { HiMiniBars3CenterLeft, HiOutlineShoppingCart } from "react-icons/hi2";
-import { IoSearchOutline } from "react-icons/io5";
-import { LuUserRound } from "react-icons/lu";
-import { FaRegHeart } from "react-icons/fa6";
-import avatarimg from "../assets/avatar.png"
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { HiOutlineBars3CenterLeft } from "react-icons/hi2";
+import { IoSearch } from "react-icons/io5";
+import { HiOutlineUser } from "react-icons/hi2";
+import { HiOutlineHeart } from "react-icons/hi2";
+import { HiOutlineShoppingCart } from "react-icons/hi";
 
-const navigation = [
-    {name: "Dashboard", href:"/user-dashboard"},
-    {name: "Orders", href:"/orders"},
-    {name: "Cart Page", href:"/cart"},
-    {name: "Check Out", href:"/checkout"},
+
+const navigation=[
+    {name:"Dashboard",href:"/dashboard"},
+    {name:"Orders", href:"/order"},
+    {name:"Cart Page", href:"/cart"},
+    {name:"Check Out" , href:"/checkout"},
 ]
 
+import avatarImg from "../assets/avatar.png"
+import { useSelector } from 'react-redux';
+
 const Navbar = () => {
-    const [isDropdownOpen, setIsDropdownOpen] = useState(false)  
+
+    const [isDropdownOpen, setIsDropdownOpen]=useState(false);
+
     console.log(isDropdownOpen)
-    const currentUser = true;
+    const cartItems = useSelector((state) => state.cart.cartItems);
+
+    
+    
+    const currentUser=true;
+
+
   return (
-    <header className='max-w-screen-2xl mx-auto px-4 py-6'>
-        <nav className='flex justify-between items-center'>
-          <div className='flex items-center md:gap-16 gap-4'> 
-            <Link to="/">
-            <HiMiniBars3CenterLeft className='size-6'/>
-
-            </Link>
-            <div className="relative sm:w-72 w-40 space-x-2">
-            <IoSearchOutline className='absolute inline-block left-3 inset-y-2'/>
-            <input type="text" placeholder='Search here' className='bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none' />
+    <header className= "max-w-screen-2xl mx-auto px-4 py-6">
+        <nav className="flex justify-between items-center">
+            <div className="flex items-center md:gap-16 gap-4">
+                <Link to="/">
+                <HiOutlineBars3CenterLeft className="size-6" />
+                </Link>
+                <div className="relative sm:w-72 w-40 space-x-2">
+                <IoSearch className="absolute inline-block left-3 inset-y-2" />  
+                <input type="text" placeholder="Search here" 
+                className="bg-[#EAEAEA] w-full py-1 md:px-8 px-6 rounded-md focus:outline-none"
+                />
+               </div>
             </div>
-            </div> 
+            <div className="relative flex items-center md:space-x-3 space-x-2 ">
+                <div>
+                    {
+                        currentUser? <><button onClick={()=>setIsDropdownOpen(!isDropdownOpen)}>
+                            <img src={avatarImg} alt="" className={`size-7 rounded-full ${currentUser ? 'ring-2 ring-blue-500':''}`}/>
+                            </button>
 
-          <div className="relative flex items-center md:space-x-3 space-x-2">
-          <div >
+                            {
+                                isDropdownOpen && (
+                                    <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
+                                        <ul className="py-2">
+                                            {
+                                            navigation.map((item)=>(
+                                                <li key={item.name} onClick={()=> setIsDropdownOpen(false)}>
+                                                <Link to={item.href} className="block px-4 py-2 text-sm hover:bg-gray-100">
+                                                {item.name}                                       
+                                                 </Link>
+                                                </li>
+
+                                            ))
+                                            }
+                                        </ul>
+                                    </div>
+                                )
+                            }
+                            
+                            
+                            
+                            
+                            </>: <Link to="/login"><HiOutlineUser  className="size-6"/></Link>
+                    }
+                </div>
+           
+            <button className="hidden sm:block">
+                
+            <HiOutlineHeart  className="size-6"/>
+            </button>
+            <Link to="/cart" className="bg-primary p-1 sm:px-6 px-2 flex items-center rounded-sm">
+
+            <HiOutlineShoppingCart  className=""/>
             {
-                currentUser ? <>
-                <button onClick={() => setIsDropdownOpen (!isDropdownOpen)}>
-                     <img src={avatarimg} alt="" className= {`size-7 rounded-full ${currentUser ? 'ring-2 ring-blue-500' : ''}`}  /> 
-                </button>
-                {
-                    isDropdownOpen && (
-                        <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-md z-40">
-                            <ul className="py-2">
-                                {
-                                    navigation.map((item) => (
-                                        <li key={item.name} onClick={() => setIsDropdownOpen(false) }>
-                                            <Link to={item.href} className="block px-4 py-2 text-sm hover:bg-gray-100">
-                                                {item.name}
-                                            </Link>
-                                        </li>
-                                    ))
-                                }
-                            </ul>
-                        </div>
-                    )
-                }
-                </> : <Link to="/login"><LuUserRound className="size-6"/></Link>
+             cartItems.length>0 ?    <span className="text-sm font-semibold sm:ml-1">{cartItems.length}</span> : <span className="text-sm font-semibold sm:ml-1">0</span>
             }
-          </div>
-          
-          <button className="hidden sm:block">
-          <FaRegHeart className='size-6'/>
-          </button>
-          <Link to="/cart" className='bg bg-primary p-1 sm:px-6 px-2 flex items-center rounded-sm'>
-          <HiOutlineShoppingCart className='size-6'/>
-          <span className="text-sm font-semibold sm:ml-1">0</span>
-          </Link>
-          </div>
+           
+            </Link>
+            </div>
         </nav>
     </header>
   )
