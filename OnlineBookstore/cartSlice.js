797273dbef4ce190383be22b3ee9203fbc9ba38e
Tanha1627 <151685331+Tanha1627@users.noreply.github.com>
2025-03-17 02:09:11 +0600

    Add files via upload

---
+++ OnlineBookstore/cartSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState={
+    cartItems: [],
+}
+
+const cartSlice = createSlice({
+    name: 'cart',
+    initialState:initialState,
+    reducers:{
+        addToCart:(state,action)=>{
+            const existingItem =state.cartItems.find(item=>item._id === action.payload._id);
+            if(!existingItem){
+                state.cartItems.push(action.payload);
+                alert("Item added successfully")
+             
+            }
+        },
+    },
+});
+
+
+//export
+
+
+export const {addToCart} = cartSlice.actions;
+export default cartSlice.reducer;
