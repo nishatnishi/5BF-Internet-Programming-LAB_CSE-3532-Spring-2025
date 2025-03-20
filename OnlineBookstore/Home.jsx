0ea780abc124503c221bdae037e973cc0585e0ff
Muskan <jnmuskanazan@gmail.com>
2025-03-20 20:35:47 +0600

    Update Home.jsx
    import Recommened from './recommened'
    <Recommened/>
    
    added by 
    C231470

--- OnlineBookstore/Home.jsx
+++ OnlineBookstore/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Banner from './Banner'
 import TopSellers from './TopSellers'
-
+import Recommened from './recommened'
 const Home = () => {
   return (
     <>
     <Banner/>
     <TopSellers/>
+      <Recommened/>
     </>
   )
 }
