d23d3f37f426d8ecb029f7824d5dd71e4ecf0391
nishatnishi <sanzidanishat76@gmail.com>
2025-06-28 23:33:15 +0600

    Add files via upload

---
+++ The Circle/BackEnd/login.php
@@ -0,0 +1,20 @@
+<?php
+include 'db.php';
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql = "SELECT * FROM users WHERE email = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("s", $email);
+$stmt->execute();
+$result = $stmt->get_result();
+$user = $result->fetch_assoc();
+
+if ($user && password_verify($password, $user['password'])) {
+  echo "Login successful";
+} else {
+  echo "Invalid email or password";
+}
+$conn->close();
+?>
